343aae826e4ce33824e695468d4c8cf897e60cdd
raihan243026 <raihanchowdhury20004@gmail.com>
2025-05-19 20:20:47 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let body = document.getElementById("body");
+
+body.style.backgroundColor =  '#FAEBD7'
+body.style.display = "flex"
+body.style.flexDirection = "column"
+body.style.alignItems = "center"
+
+let header = document.getElementById("header");
+    header.style.backgroundColor = "red";
+    header.style.color = "white";
+    header.style.padding = "10px";
+    header.style.textAlign = "center";
+
+// let parent = document.getElementById("father")
+ let father = document.getElementById('Father');
+father.style.display = "flex";
+  father.style.flexWrap = "wrap";
+  father.style.justifyContent = "center"; 
+  father.style.gap = "20px";
+
+//  father.style.display = 'grid';
+//   father.style.gridTemplateColumns = 'repeat(auto-fit, minmax(200px, 1fr))';
+  father.style.gap = '20px'
+
+// let body = document.getElementById("body");
+let itemLists = document.getElementById( 'item-List');
+
+    itemList.style.backgroundColor = 'white';
+    itemList.style.border = '1px solid rgb(0, 0, 0)'; 
+    itemList.style.borderRadius = '5px';
+    itemList.style.padding = '15px';
+    itemList.style.boxShadow = '2px 2px 5px rgb(0, 0, 0)';
